0d26a6e68bf9aa92e91a8a26e153a1867c34f403
Cbodtorf <cbodtorf@gmail.com>
2016-06-27 12:13:54 -0400

    initial file structure

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var babel = require('gulp-babel');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+
+// Default runner
+gulp.task('default', ['html','css','js']);
+
+gulp.task('html', function() {
+    gulp.src('./index.html')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('css', function() {
+    gulp.src('./css/styles.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('js', function() {
+  gulp.src('./js/main.js')
+  .pipe(babel({
+			presets: ['es2015']
+		}))
+  .pipe(browserify())
+  .pipe(gulp.dest('./public'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./css/*.scss', ['css']);
+  gulp.watch('./index.html', ['html']);
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./js/*/*.js', ['js']);
+});
